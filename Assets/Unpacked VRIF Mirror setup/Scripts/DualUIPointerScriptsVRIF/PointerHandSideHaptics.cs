31b5f38b8a8a6a30dab8f55b6c194d8333f0cac6
jens <jensmonne@gmail.com>
2025-06-20 14:12:52 +0200

    gfhuoi
    w4rp

--- Assets/Unpacked VRIF Mirror setup/Scripts/DualUIPointerScriptsVRIF/PointerHandSideHaptics.cs
+++ Assets/Unpacked VRIF Mirror setup/Scripts/DualUIPointerScriptsVRIF/PointerHandSideHaptics.cs
@@ -4,9 +4,9 @@ using BNG;
 public class PointerHandSideHaptics : MonoBehaviour
 {
     [Header("Controller Haptic Settings")]
-    public float VibrateFrequency = 0.3f;
-    public float VibrateAmplitude = 0.1f;
-    public float VibrateDuration = 0.1f;
+    public float VibrateFrequency = 1f;
+    public float VibrateAmplitude = 1f;
+    public float VibrateDuration = 2f;
     // run this from PointerEvents Component on the object
     VRUISystem vrUI;
     
