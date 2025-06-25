37f220f4aeda14b204f89fc9124dbf07600431fe
jens <jensmonne@gmail.com>
2025-06-25 10:53:15 +0200

    bbunch more stuff

--- Assets/Scripts/UI/CanvasSwitcher.cs
+++ Assets/Scripts/UI/CanvasSwitcher.cs
@@ -4,16 +4,26 @@ public class CanvasSwitcher : MonoBehaviour
 {
     [SerializeField] private GameObject menu;
     [SerializeField] private GameObject settings;
+    [SerializeField] private GameObject connectUI;
 
     public void SettingsButton()
     {
         menu.SetActive(false);
         settings.SetActive(true);
+        connectUI.SetActive(false);
     }
 
     public void Back()
     {
         menu.SetActive(true);
         settings.SetActive(false);
+        connectUI.SetActive(false);
+    }
+    
+    public void StartConnect()
+    {
+        menu.SetActive(false);
+        settings.SetActive(false);
+        connectUI.SetActive(true);
     }
 }
