40e4929de21ee5a6f8bb93104c5640741469c9db
jens <jensmonne@gmail.com>
2025-06-26 11:41:02 +0200

    gjbir;

--- Assets/Scripts/UI/ButtonStuff.cs
+++ Assets/Scripts/UI/ButtonStuff.cs
@@ -2,10 +2,14 @@ using Mirror;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ButtonStuff : MonoBehaviour
+public class ButtonStuff : NetworkBehaviour
 {
     public void MainMenu()
     {
+        if (isClient) NetworkManager.singleton.StopClient();
+
+        if (isServer) NetworkManager.singleton.StopHost();
+        
         SceneManager.LoadScene("MainMenu");
     }
     
@@ -15,6 +19,11 @@ public class ButtonStuff : MonoBehaviour
         // Change the scene for all clients
         //  ServerChangeScene(sceneToLoad);
         NetworkManager.singleton.ServerChangeScene("Level1");
+    }
+
+    public void SceneChange()
+    {
+        SceneManager.LoadScene("Level1Offline");
     }
     
     public void Quit()
