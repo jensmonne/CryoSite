58adc3fdedeae4f69d52532407312ac4b8efd05a
jens <jensmonne@gmail.com>
2025-07-10 09:56:56 +0200

    nullreference

--- Assets/Scripts/Core/VoiceOverPlay.cs
+++ Assets/Scripts/Core/VoiceOverPlay.cs
@@ -1,18 +1,16 @@
-using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class VoiceOverPlay : MonoBehaviour
 {
     [SerializeField] private AudioSource[] audioSource;
-
-
+    
     private void Start()
     {
+        if (audioSource == null || audioSource.Length == 0) return;
+        
         int randomIndex = Random.Range(0, audioSource.Length);
-        
         AudioSource selectedAudio = audioSource[randomIndex];
-        
         selectedAudio.Play();
     }
 }
